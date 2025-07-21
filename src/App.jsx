b21096dd3cc5310edee06de6a2abeb3b46373589
prashantkumar999 <prashant047alg@gmail.com>
2025-07-21 17:04:53 +0530

    basic functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [apiData, setApiData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [start, setStart] = useState(0)
+  const [end, setEnd] = useState(start + 5)
+  const totalItems = apiData.length;
+  const singlePageItems = 5;
+  const totalPages = Math.ceil(totalItems / singlePageItems)
+  console.log(totalPages)
+
+  const buttonClickHandler = (index) => {
+    setCurrentPage(index + 1)
+    setStart(index * singlePageItems)
+    setEnd((index + 1) * singlePageItems);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch('https://dummyjson.com/products')
+        const jsonData = await data.json()
+        setApiData(jsonData.products); // we only need products 
+
+      } catch (err) {
+        console.error(err)
+      }
+
+    }
+    fetchData();
+  }, [])
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='main-container'>
+        <div className='pagination'>
+          {
+            Array.from({ length: totalPages }, (_, index) => (
+              <div onClick={() => buttonClickHandler(index)} >
+                <button className={currentPage==(index+1)?'active':''}>{index + 1}</button>
+              </div>
+            ))
+          }
+        </div>
+        <div className='product-container'>{
+          apiData.slice(start, end).map((data) => (
+            <div key={data.id} className='product-card'>
+              <p>{data.title}</p>
+              <img src={data.thumbnail} />
+            </div>
+          ))
+        }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
